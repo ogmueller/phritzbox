634308593af39df2b3fe6d91bd8abc4a279fc144
Oliver G. Mueller <oliver@teqneers.de>
2020-12-23 23:22:28 +0100

    added config files, docker, entities

---
+++ src/Entity/SmartDeviceData.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\SmartDeviceDataRepository")
+ */
+class SmartDeviceData
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $dataId;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $sid;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $type;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $time;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $value;
+
+    public function getDataId(): ?int
+    {
+        return $this->dataId;
+    }
+
+    public function setDataId(int $dataId): self
+    {
+        $this->dataId = $dataId;
+
+        return $this;
+    }
+
+    public function getSid(): ?string
+    {
+        return $this->sid;
+    }
+
+    public function setSid(string $sid): self
+    {
+        $this->sid = $sid;
+
+        return $this;
+    }
+
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
+
+    public function setType(string $type): self
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    public function getTime(): ?\DateTimeInterface
+    {
+        return $this->time;
+    }
+
+    public function setTime(\DateTimeInterface $time): self
+    {
+        $this->time = $time;
+
+        return $this;
+    }
+
+    public function getValue(): ?float
+    {
+        return $this->value;
+    }
+
+    public function setValue(float $value): self
+    {
+        $this->value = $value;
+
+        return $this;
+    }
+}
